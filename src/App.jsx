45ac6e405b098d4c1217cba0ab2b2442bf352570
Vika Sabadosh <vika.kahanec@gmail.com>
2024-11-08 16:11:13 +0200

    update request

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=61d31267`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=61d31267`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
